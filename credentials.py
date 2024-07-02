54477f96fab59b42c4147249ce3bfa2074681ff4
Artur Primo <99571960+ArturPrimo@users.noreply.github.com>
2024-07-02 16:39:17 -0300

    Update

--- credentials.py
+++ credentials.py
@@ -4,3 +4,5 @@ os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = "-"
 os.environ["GCP_API_KEY"] = "-"
 os.environ["GCP_APP_ID"] = "-"
 os.environ["PROJECT_ID"] = "-"
+
+#keys and credentials for GCP have been removed for security and privacy purposes.
